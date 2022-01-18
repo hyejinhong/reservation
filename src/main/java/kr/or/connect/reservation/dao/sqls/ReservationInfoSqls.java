9141303feb8e455b6b6eec4db403aec789424581
hyejinhong <hhj732@naver.com>
2022-01-18 21:44:50 +0900

    GET ReservationInfo 중복 해결

--- src/main/java/kr/or/connect/reservation/dao/sqls/ReservationInfoSqls.java
+++ src/main/java/kr/or/connect/reservation/dao/sqls/ReservationInfoSqls.java
@@ -5,7 +5,7 @@ public class ReservationInfoSqls {
 			"SELECT id, product_id, cancel_flag, display_info_id, user_id, reservation_date, create_date, modify_date FROM reservation_info WHERE id=:id";
 
 	public static final String SELECT_BY_USER_ID =
-			"SELECT info.id, info.product_id, info.display_info_id, info.cancel_flag, product.description AS productDescription, product.content AS productContent, info.user_id, info.reservation_date, info.create_date, info.modify_date, reservation_price.count*product_price.price AS sumPrice FROM reservation_info AS info JOIN reservation_info_price AS reservation_price ON info.id=reservation_price.reservation_info_id LEFT JOIN product_price ON reservation_price.product_price_id=product_price.id LEFT JOIN product ON product_price.product_id = product.id WHERE info.user_id=:user_id";
+			"SELECT info.id, info.product_id, info.display_info_id, info.cancel_flag, product.description AS productDescription, product.content AS productContent, info.user_id, info.reservation_date, info.create_date, info.modify_date FROM reservation_info AS info LEFT JOIN reservation_info_price AS rip ON info.id=rip.reservation_info_id RIGHT JOIN product_price AS pp ON rip.product_price_id=pp.id LEFT JOIN product ON pp.product_id = product.id GROUP BY rip.reservation_info_id HAVING info.user_id=:user_id";
 	
 	public static final String UPDATE_BY_ID =
 			"UPDATE reservation_info SET cancel_flag=1 WHERE id=:id";
